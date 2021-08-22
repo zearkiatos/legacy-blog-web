eaaa769bc198127cd89ae3c4abd2d927cb03bfa8
zearkiatos <caprilespe@outlook.com>
2021-08-22 17:14:56 -0400

    Initialization base project

---
+++ README.md
@@ -0,0 +1,2 @@
+## Description
+Legacy web react ⚛️ blog web application for public post article from different users and topics
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>
+        Hello World! ⚛️ 🌎
+      </h1>
+    );
+  }
+}
