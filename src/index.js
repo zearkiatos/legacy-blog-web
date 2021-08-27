c54a77c33199d662be7cc4d7296e6adf3558f31e
Pedro Capriles M <p0c04cw@M-C02Z9029LVCG.local>
2021-08-27 08:37:01 -0400

    The Basics of React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Router, Route, Switch } from "react-router-dom";
 
 import App from "./components/App";
+import history from './history';
+import reducers from "./reducers";
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render() {
+    return <div>Hello!</div>;
+  }
+}
+
+class GoodBye extends React.Component {
+  render() {
+    return <div>Goodbye!</div>;
+  }
+}
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={history}>
+      <div>
+        Header
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={GoodBye} />
+      </div>
+    </Router>
+  </Provider>,
   document.querySelector(".container")
 );
