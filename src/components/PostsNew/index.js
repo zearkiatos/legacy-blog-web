0b10f2763bce3e194307ab1f77b00dd32464f019
zearkiatos <caprilespe@outlook.com>
2021-09-04 20:23:25 -0400

    Setting Up Redux Form and fix github action

--- src/components/PostsNew/index.js
+++ src/components/PostsNew/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
     render() {
-        return <div>PostsNew</div>;
+        return (
+            <form>
+                <Field
+                    name="title"
+                    component={}
+                />
+            </form>
+        );
     }
 }
 
-export default PostsNew
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
