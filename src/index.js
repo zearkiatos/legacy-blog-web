12c8c7eddb06ea6087fd3fe13482795fedaf511e
zearkiatos <caprilespe@outlook.com>
2021-09-11 13:22:18 -0300

    The Posts Show Component and fix github actions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import promise from 'redux-promise';
+import promise from "redux-promise";
 
 import reducers from "./reducers";
 import PostsIndex from "./components/PostsIndex";
 import PostsNew from "./components/PostsNew";
+import PostsShow from "./components/PostsShow";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -17,6 +18,7 @@ ReactDOM.render(
       <div>
         <Switch>
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/post/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
