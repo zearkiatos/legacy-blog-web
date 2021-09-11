cbabd8f5b8c706d198e8d4fdd4533fdc405718af
zearkiatos <caprilespe@outlook.com>
2021-09-11 18:31:35 -0300

    Selecting from OwnProps and gitlab-ci fix

--- src/components/PostsShow/index.js
+++ src/components/PostsShow/index.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-
+import { connect } from 'react-redux';
+import { postActions } from "../../actions";
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  helperFunction() {
+    this.props.posts[this.props.match.params.id];
+  }
+
   render() {
     return <div>Posts Show</div>;
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, {
+  fetchPost: postActions.fetchPost,
+})(PostsShow);
