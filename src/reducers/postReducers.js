391815dc9fb1b2b7ad5a14d1243ab25d9df6556a
zearkiatos <caprilespe@outlook.com>
2021-09-15 22:19:17 -0300

    Wrapup

--- src/reducers/postReducers.js
+++ src/reducers/postReducers.js
@@ -1,4 +1,4 @@
-import { mapKeys } from "lodash";
+import { mapKeys, omit } from "lodash";
 import { POST_TYPES } from "../types";
 
 const postReducers = (state = {}, action) => {
@@ -7,6 +7,8 @@ const postReducers = (state = {}, action) => {
       return mapKeys(action.payload.data, "id");
     case POST_TYPES.FETCH_POST:
       return {...state, [action.payload.data.id]: action.payload.data};
+    case POST_TYPES.DELETE_POST:
+      return omit(state, action.payload);
     default:
       return state;
   }
